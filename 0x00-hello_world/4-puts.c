a7ec6d379adf3cfa69b741401196f9d7d0a0b7a4
Beekola11 <sunkanmi7999@gmail.com>
2022-06-02 15:16:18 -0700

    low level language script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - Entry mains
+ * 
+ * This programprints a string without thr printf
+ * Return: 
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+        
+		return (0);
+
+}
