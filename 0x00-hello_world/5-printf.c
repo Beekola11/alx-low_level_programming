8e7b6e960824862b324f26ba2da8f05c22759aec
Beekola11 <sunkanmi7999@gmail.com>
2022-06-02 16:43:59 -0700

    low level language script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-/** main: inpute
- *
+/* main: inpute
  * this file print using puts
- *
  * returs(0)
  */
 int main(void){
- printf("with proper grammar, but the outcome is a piece of art,\n");
-
- return(0);
+ int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); } 
 }
