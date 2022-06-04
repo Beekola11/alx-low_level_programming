d1211fc847cd307d3aada1587792284c7752013e
Beekola11 <sunkanmi7999@gmail.com>
2022-06-03 21:37:57 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10 starting from
+ * All your code should be in the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+int x;
+while (x <= 10)
+{
+printf("%d\n", x);
+x += 1;
+}
+return (0);
+}
