d0b28923c7d4a7f434c8172569aa92cb07836848
Beekola11 <sunkanmi7999@gmail.com>
2022-06-03 21:13:12 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase, and then in uppercase
+ * You can only use the putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+char lower_c;
+char upper_c;
+
+for (lower_c = 'a'; lower_c <= 'z'; lower_c++)
+for (upper_c = 'A'; upper_c <= 'Z'; upper_c++)
+{
+putchar(lower_c);
+putchar(upper_c);
+}
+putchar('\n');
+return (0);
+}
